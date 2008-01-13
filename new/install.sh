8968c6c0c4d6ac4e706c694415b493468ef9a738
oldherl <oldherl@63941770-9543-0410-8ac1-c6342061d5de>
2008-01-13 13:29:47 +0000

    a "prepare" script and a "install.sh" script for devel use now. Still need improving or rewrite using autoconf/automake.
    git-svn-id: https://svn.code.sf.net/p/lowsars/code@6 63941770-9543-0410-8ac1-c6342061d5de

---
+++ new/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Note: this is only for developers, since this can only work on OSes very similar to mine.
+# TODO: (very hard) rewrite it using autoconf/automake
+
+d_share=/usr/share
+echo "Copying the share files..."
+mkdir -p $d_share/lowsars
+cp lib/* $d_share/lowsars/
+echo "Copying the i18n files..."
+d_locale=/usr/share/locale
+langs=`cat po/langs`
+f=lowsars
+for lang in $langs; do
+   mkdir -p $d_locale/$lang/LC_MESSAGES/
+   cp po/$lang/$f.mo $d_locale/$lang/LC_MESSAGES/
+done
+echo "Copying the lowsars-* files..."
+d_bin=/usr/bin
+mkdir -p $d_bin
+binfiles=`ls lowsars*`
+for bf in $binfiles; do
+   cp $bf $d_bin/$bf
+done
+echo "All done."
+
