a3add35166bd699eb970338747198d91d8337540
oldherl <oldherl@63941770-9543-0410-8ac1-c6342061d5de>
2008-04-13 02:53:51 +0000

    bug on special judge!
    git-svn-id: https://svn.code.sf.net/p/lowsars/code@31 63941770-9543-0410-8ac1-c6342061d5de

--- trunk/install.sh
+++ trunk/install.sh
@@ -2,27 +2,27 @@
 # Note: this is only for developers, since this can only work on OSes very similar to mine.
 # TODO: (very hard) rewrite it using autoconf/automake
 
-d_share=/usr/share
+D_BIN=/usr/bin/
+D_SHARE=/usr/share/lowsars/
+BINFILES="lowsars lowsars-test lowsars-input lowsars-commander"
+LIBFILES="lib/database.sh lib/errorcodes-fp lib/escape.sh"
 echo "Copying the share files..."
-mkdir -p $d_share/lowsars
-cp lib/* $d_share/lowsars/
+mkdir -p $D_SHARE
+cp $LIBFILES $D_SHARE
 echo "Copying the i18n files..."
-d_locale=/usr/share/locale
-langs=`cat po/langs`
-f=lowsars
-for lang in $langs; do
-   mkdir -p $d_locale/$lang/LC_MESSAGES/
-   cp po/$lang/$f.mo $d_locale/$lang/LC_MESSAGES/
+D_LOCALE=/usr/share/locale
+LANGS=zh_CN
+POFILE=lowsars
+for lang in $LANGS; do
+   mkdir -p $D_LOCALE/$lang/LC_MESSAGES/
+   cp po/$lang/$POFILE.mo $D_LOCALE/$lang/LC_MESSAGES/
 done
 echo "Copying the lowsars-* files..."
-d_bin=/usr/bin
-mkdir -p $d_bin
-binfiles=`ls lowsars*`
-for bf in ${binfiles//~/}; do
-   cp $bf $d_bin/$bf
-done
-echo "Copying /bin/bash to /bin/bash_by_lowsars"
+cp $BINFILES $D_BIN
 echo "If anything goes wrong, try copy a bash from Ubuntu to /bin/bash_by_lowsars"
-# cp /bin/bash /bin/bash_by_lowsars
+[ -x /bin/bash_by_lowsars ]||{
+   echo "Copying /bin/bash to /bin/bash_by_lowsars"
+   cp /bin/bash /bin/bash_by_lowsars
+}
 echo "All done."
 
